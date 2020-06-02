9c62692d09e2eaa0bcfbb52e9f0d8e3f5c072e2b
mansurissa <mansurissa6@gmail.com>
2020-06-02 20:25:01 +0200

    making ready for deployment

---
+++ sm-front-end/build/precache-manifest.debb474ad9a37172c6e8abbfc8a8f547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a822f4c8e63968a17cf4d5917b951c",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "48266c2a36cea599b5ab",
+    "url": "/smart-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1fe0da3be1d59918657b",
+    "url": "/smart-brain/static/css/main.ac3283de.chunk.css"
+  },
+  {
+    "revision": "48266c2a36cea599b5ab",
+    "url": "/smart-brain/static/js/2.2c8b5e86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.2c8b5e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe0da3be1d59918657b",
+    "url": "/smart-brain/static/js/main.b856b012.chunk.js"
+  },
+  {
+    "revision": "07687dc78c01b19d353b",
+    "url": "/smart-brain/static/js/runtime-main.cf82cd7f.js"
+  }
+]);
