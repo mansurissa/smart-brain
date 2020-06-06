b88c1af0f590b03dc39d38ab474cebf8d99fad6c
mansurissa <mansurissa6@gmail.com>
2020-06-06 12:14:10 +0200

    adding a user guide text

--- sm-front-end/src/components/rank/rank.js
+++ sm-front-end/src/components/rank/rank.js
@@ -3,6 +3,9 @@ import React from 'react'
 const rank = ({ username, entries }) => {
     return (
         <div className=' white '>
+            <div className="f2 center">
+                Paste a link of a photo that you want to detect face in. and let's the magic happen!
+            </div>
             <div className='f3 center'>
                 {`Dear, ${username} your current detected faces are:`}
             </div>
